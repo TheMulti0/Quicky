da81dd1e742c668293592770d47f10466c5c1330
TheMulti0 <raphael@codeprecise.com>
2018-01-20 14:27:34 +0200

    neww

---
+++ QuickyApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickyApp.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        public static 
+    }
+}
---
+++ QuickyApp/Views/Tile.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace QuickyApp.Views
+{
+    /// <summary>
+    ///     Interaction logic for Tile.xaml
+    /// </summary>
+    public partial class Tile : UserControl
+    {
+        public static readonly DependencyProperty AccentColorProperty = DependencyProperty.Register(
+            "AccentColor", typeof(Brush), typeof(Tile), new PropertyMetadata(Brushes.Black));
+
+        public Tile()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public Brush AccentColor
+        {
+            get => (Brush) GetValue(AccentColorProperty);
+            set => SetValue(AccentColorProperty, value);
+        }
+
+        public string Icon { get; set; }
+
+        public string Text { get; set; }
+
+        public string Description { get; set; }
+    }
+}
